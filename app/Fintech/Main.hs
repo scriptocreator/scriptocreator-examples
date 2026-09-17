11d3411acb22c55efdc8ef3058c23cdc2c53074e
Andrey Boyarkin <90903451+scriptocreator@users.noreply.github.com>
2026-09-17 05:28:03 +0400

    [Fintech] Seems to decide / Вроде решает

--- app/Fintech/Main.hs
+++ app/Fintech/Main.hs
@@ -6,21 +6,32 @@ import Text.Read (readMaybe)
 
 
 newtype Процент a = Процент a deriving (Show, Eq, Ord, Read)
-newtype Заказ a = Заказ a deriving (Show, Eq, Ord, Read)
+data Заказ a = Заказ a | Заказ' a a deriving (Show, Eq, Ord, Read)
 
 
-allowance :: Процент Double -> Заказ Double -> (Double, Double)
-allowance (Процент percent) (Заказ order)
-    = (order + sum, sum)
+num % perc = (num / 100) * perc
 
-    where sum = order / 100 * percent
+allow (Заказ orig) mPerc@(Процент perc) =
+    let newOrder = Заказ' orig orig
+    in allow newOrder mPerc
+
+allow (Заказ' orig transOrig) mPerc@(Процент perc)
+    | alligTotal >= orig = (curTransTotal, gap)
+    | otherwise =
+        let correctX = Заказ' orig newTransOrig
+        in allow correctX mPerc
+
+    where curTransTotal = transOrig + (transOrig % perc)
+          newTransOrig = transOrig + (orig - alligTotal)
+          alligTotal = curTransTotal - (curTransTotal % perc)
+          gap = curTransTotal - orig
 
 -- => лево = fst; право = snd
--- => счёт = allowance (Процент 2) (Заказ 1870)
+-- => счёт = allow (Заказ 1870) (Процент 2)
 -- => счёт
--- (1907.4,37.4)
+-- (1908.1632653061224,38.16326530612241)
 -- => лево счёт - право счёт
--- 1907.4
+-- 1908.1632653061224
 
 
 main :: IO ()
@@ -29,7 +40,7 @@ main = do
 
     let mAll = case arg of
             (a:b:_) ->
-                let (mRA, mRB) = (readMaybe a, readMaybe b) :: (Maybe (Процент Double), Maybe (Заказ Double))
+                let (mRA, mRB) = (readMaybe a, readMaybe b) :: (Maybe (Заказ Double), Maybe (Процент Double))
                     rAB = (fromJust mRA, fromJust mRB)
 
                 in if isJust mRA && isJust mRB
@@ -38,8 +49,8 @@ main = do
 
             _ -> Nothing
 
-        (perc, ord) = fromJust mAll
+        (ord, perc) = fromJust mAll
     
     if isJust mAll
-        then print $ allowance perc ord
+        then print $ allow ord perc
         else return ()
