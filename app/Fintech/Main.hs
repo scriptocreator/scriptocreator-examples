c18dd0cfab551aa6ee0c282666f79ba391bfcc07
Andrey Boyarkin <90903451+scriptocreator@users.noreply.github.com>
2026-09-18 05:36:21 +0400

    [Fintech] Improved notation / Улучшенная нотация

--- app/Fintech/Main.hs
+++ app/Fintech/Main.hs
@@ -4,32 +4,74 @@ import System.Environment (getArgs)
 import Data.Maybe (isJust, fromJust)
 import Text.Read (readMaybe)
 import Text.Printf
+import Data.Word
 
 
 newtype Процент a = Процент a deriving (Show, Eq, Ord, Read)
 data Заказ a = Заказ a | Заказ' a a deriving (Show, Eq, Ord, Read)
 
 
+data Mode a
+    = Run
+    | Stop
+    | Debug !a
+    deriving (Show, Eq, Ord, Read)
+
+class NumFunctor f where
+  nmap :: (Num a, Eq a) => (a -> a) -> f a -> f a
+
+instance NumFunctor Mode where
+    nmap f (Debug 0) = Stop
+    nmap f (Debug calc) = Debug (f calc)
+    nmap _ Stop = Stop
+    nmap _ Run = Run
+
+
+data Step = Step
+    { curTransTotal :: !Double
+    , newTransOrig  :: !Double
+    , alligTotal    :: !Double
+    , gap           :: !Double
+    }
+
+compute :: Заказ Double -> Процент Double -> Step
+compute (Заказ' orig transOrig) (Процент perc)
+    = Step curTransTotal newTransOrig alligTotal gap
+    
+    where curTransTotal = transOrig + (transOrig % perc)
+          newTransOrig  = transOrig + (orig - alligTotal)
+          alligTotal    = curTransTotal - (curTransTotal % perc)
+          gap           = curTransTotal - orig
+
+
+(%) :: Double -> Double -> Double
 num % perc = (num / 100) * perc
 
-allow :: Заказ Double -> Процент Double -> Int -> (Double, Double)
+infixl 1 $.
+($.) :: (a -> b) -> a -> b
+f $. x = f x
 
-allow (Заказ orig) mPerc@(Процент perc) calc =
-    let newOrder = Заказ' orig orig
-    in allow newOrder mPerc calc
 
-allow (Заказ' orig transOrig) mPerc@(Процент perc) calc
-    | calc == 0 = error $ printf
-        "curTransTotal %f, newTransOrig %f, alligTotal %f, gap %f" curTransTotal newTransOrig alligTotal gap
-    | alligTotal >= orig = (curTransTotal, gap)
-    | otherwise =
-        let correctX = Заказ' orig newTransOrig
-        in allow correctX mPerc (pred calc)
+allow :: Заказ Double -> Процент Double -> Mode Word -> (Double, Double)
 
-    where curTransTotal = transOrig + (transOrig % perc)
-          newTransOrig = transOrig + (orig - alligTotal)
-          alligTotal = curTransTotal - (curTransTotal % perc)
-          gap = curTransTotal - orig
+allow (Заказ orig) perc calc = allow (Заказ' orig orig) perc calc
+
+allow order perc Stop = error $ printf
+    $. "curTransTotal = %f, newTransOrig = %f, alligTotal = %f, gap = %f"
+    $. curTransTotal s
+    $. newTransOrig s
+    $. alligTotal s
+    $. gap s
+
+    where s = compute order perc
+
+allow order@(Заказ' orig _) perc calc
+    | alligTotal s >= orig = (curTransTotal s, gap s)
+    | otherwise = allow correctX perc newCalc
+
+    where s = compute order perc
+          correctX = Заказ' orig (newTransOrig s)
+          newCalc = nmap pred calc
 
 -- => лево = fst; право = snd
 -- => счёт = allow (Заказ 1870) (Процент 2)
@@ -42,21 +84,14 @@ allow (Заказ' orig transOrig) mPerc@(Процент perc) calc
 
 main :: IO ()
 main = do
-    arg <- getArgs
+    args <- getArgs
+    case args of
+        (a:b:_) ->
+            let mOrdPerc :: (Maybe (Заказ Double), Maybe (Процент Double))
+                mOrdPerc = (readMaybe a, readMaybe b)
 
-    let mAll = case arg of
-            (a:b:_) ->
-                let (mRA, mRB) = (readMaybe a, readMaybe b) :: (Maybe (Заказ Double), Maybe (Процент Double))
-                    rAB = (fromJust mRA, fromJust mRB)
+            in case mOrdPerc of
+                (Just ord, Just perc) -> print (allow ord perc Run)
+                _                     -> pure ()
 
-                in if isJust mRA && isJust mRB
-                    then return rAB
-                    else Nothing
-
-            _ -> Nothing
-
-        (ord, perc) = fromJust mAll
-    
-    if isJust mAll
-        then print $ allow ord perc (-1)
-        else return ()
+        _ -> pure ()
